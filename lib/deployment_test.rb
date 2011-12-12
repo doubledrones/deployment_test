c32fe49419990c2863169bed02fb9cee4a014422
Marcin Nowicki <pr0d1r2@gmail.com>
2011-12-12 10:48:04 +0100

    Workaround to work with Ruby < 1.9.2

--- lib/deployment_test.rb
+++ lib/deployment_test.rb
@@ -1,1 +1,9 @@
+unless Kernel.respond_to?(:require_relative)
+  module Kernel
+    def require_relative(path)
+      require File.join(File.expand_path(File.dirname(__FILE__)), file)
+    end
+  end
+end
+
 require_relative 'deployment_test/postgres'
